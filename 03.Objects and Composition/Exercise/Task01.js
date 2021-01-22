f5d22e86692b4dba16582860ec2023c129506926
Alexander Nestorov <67589580+AlexanderNestorov@users.noreply.github.com>
2021-01-22 13:42:12 +0200

    Added code comments.

--- 03.Objects and Composition/Exercise/Task01.js
+++ 03.Objects and Composition/Exercise/Task01.js
@@ -7,8 +7,12 @@ function solve(array) {
 // As said in the description, every even element will be a 'name' or key property
 // and every odd element will be a value corresponding to that key.
     for (let i = 0; i < array.length; i += 2) {
+        // We make a property in 'result' which is an even element to hold the odd
+        // element next to it.So the key in 'result' corresponding to the element's name
+        // will hold its value which is the element's calorie number.
        result[array[i]] = Number(array[i+1]);
     }
+ // finally we just log the object
     console.log(result);
 }
 

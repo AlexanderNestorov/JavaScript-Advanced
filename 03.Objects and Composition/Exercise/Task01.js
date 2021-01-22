130b74112e9cdf75df52476ac4a4525216265455
Alexander Nestorov <67589580+AlexanderNestorov@users.noreply.github.com>
2021-01-22 13:38:19 +0200

    Update Task01.js

--- 03.Objects and Composition/Exercise/Task01.js
+++ 03.Objects and Composition/Exercise/Task01.js
@@ -1,8 +1,11 @@
 function solve(array) {
 
-    
+//We begin by initializing the Object in which we will keep our results.    
     let result = {};
 
+// We need a simple 'for' cycle to iterate through the input array.
+// As said in the description, every even element will be a 'name' or key property
+// and every odd element will be a value corresponding to that key.
     for (let i = 0; i < array.length; i += 2) {
        result[array[i]] = Number(array[i+1]);
     }

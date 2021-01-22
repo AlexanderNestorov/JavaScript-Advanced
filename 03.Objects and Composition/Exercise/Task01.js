df188adc1990ea5864bd2da636139369c6dff970
Alexander Nestorov <67589580+AlexanderNestorov@users.noreply.github.com>
2021-01-22 13:43:54 +0200

    Added code comments

--- 03.Objects and Composition/Exercise/Task01.js
+++ 03.Objects and Composition/Exercise/Task01.js
@@ -6,6 +6,7 @@ function solve(array) {
 // We need a simple 'for' cycle to iterate through the input array.
 // As said in the description, every even element will be a 'name' or key property
 // and every odd element will be a value corresponding to that key.
+// We iterate over 2 elements to easily get an even-odd combination. 
     for (let i = 0; i < array.length; i += 2) {
         // We make a property in 'result' which is an even element to hold the odd
         // element next to it.So the key in 'result' corresponding to the element's name

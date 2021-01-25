0d8421fbf3792ee7c9975fd48f2c38e1ece2d6e1
Alexander Nestorov <jak.racer@gmail.com>
2021-01-25 22:59:09 +0200

    Uploaded solutions

---
+++ 04 DOM Introduction/Exercise/Task06/solution.js
@@ -0,0 +1,19 @@
+function solve() {
+  let inputText = document.getElementById('input').value;
+  let sentenceList = Array.from(inputText.split(". ").filter(e => e));
+  let trial = []
+  while(sentenceList.length>=3) {
+    if (sentenceList[0]=="") {
+      break;
+    }
+      trial = sentenceList.splice(0,3)
+      let paragraph = document.createElement("P")
+      paragraph.innerHTML = trial.join(".") + '.'
+       document.getElementById('output').appendChild(paragraph) 
+    }
+    if (sentenceList.length < 3 && sentenceList[0]!="") {
+      let paragraph = document.createElement("P")
+      paragraph.innerHTML = sentenceList.join(".")
+       document.getElementById('output').appendChild(paragraph) 
+    }
+  }

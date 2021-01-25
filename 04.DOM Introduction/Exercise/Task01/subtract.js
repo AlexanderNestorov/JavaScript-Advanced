ab94e3e0c7bc596b8ba399fc50fa1ce8587bc299
Alexander Nestorov <jak.racer@gmail.com>
2021-01-25 17:19:30 +0200

    Uploaded solutions

--- 04.DOM Introduction/Exercise/Task01/subtract.js
+++ 04.DOM Introduction/Exercise/Task01/subtract.js
@@ -1,3 +1,9 @@
 function subtract() {
-    console.log('TODO:...');
+    var num1 = Number(document.getElementById('firstNumber').value);
+    var num2 = Number(document.getElementById('secondNumber').value);
+    
+    const result = num1 - num2;
+
+    document.getElementById('result').textContent = result;
+
 }

a6208c86ef4cf3eb28ff951f0f65a3b4ad29f09f
Alexander Nestorov <jak.racer@gmail.com>
2021-01-25 19:23:12 +0200

    Uploaded solutions

---
+++ 04 DOM Introduction/Exercise/Task03/accordion.js
@@ -0,0 +1,8 @@
+function toggle() {
+    let button = document.querySelector('.button');
+    let divExtra = document.querySelector('#extra');
+
+    divExtra.style.display = divExtra.style.display === 'none' ? 'block' : 'none';
+
+    button.textContent = button.textContent === 'More' ? 'Less' : 'More';
+}
